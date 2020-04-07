0833eadda5b8e65e10cc86227ff99c5c14a565db
Jeff Andolora <jeeeeefff@gmail.com>
2020-04-07 13:17:19 -0400

    Removes service worker for now

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const steam = require("./steam");
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 3001;
 
 const server = http.createServer(async (request, response) => {
   const requestURL = new URL(request.url, `http://${request.headers.host}`);
