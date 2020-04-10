4976c150b5b45296939618e539a27ee55b9dc48c
Jeff Andolora <jeeeeefff@gmail.com>
2020-04-10 15:47:06 -0400

    Refactors to enable a dynamic amount of steamIds

--- client/src/App.js
+++ client/src/App.js
@@ -1,75 +1,111 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import deleteIcon from "./delete.svg";
 import Service from "./service";
 
-let service;
+const service = new Service("http://localhost:3001");
 
-const SAMPLE_STEAM_IDS = ["76561198039881304", "76561198085985595"];
+const initialState = {
+  userData: {
+    "76561198039881304": [],
+    "76561198085985595": []
+  }
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setApiData":
+      const newState = {
+        ...initialState,
+        userData: {
+          ...state.userData,
+          [action.data.steamId]: action.data.apiResponse
+        }
+      };
+
+      return newState;
+
+    case "changeSteamId":
+      //todo
+      return initialState;
+
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
-  const [apiData, setApiData] = useState([]);
-  //todo fix in next commit
-  //eslint-disable-next-line
-  const [steamIds, setSteamIds] = useState(SAMPLE_STEAM_IDS);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const steamIds = Object.keys(state.userData);
+  const steamIdHashForComparison = steamIds.join();
 
   useEffect(() => {
-    service = new Service("http://localhost:3001");
-  }, []);
+    queryAPIForGamesOwned(steamIds, dispatch);
 
-  useEffect(() => {
-    const getOwnedGames = async users => {
-      Promise.all(
-        users.map(async user => await service.getOwnedGames(user))
-      ).then(setApiData);
-    };
-
-    getOwnedGames(steamIds);
-  }, [steamIds]);
+    /* eslint-disable react-hooks/exhaustive-deps */
+    // This is needed because useEffect hooks don't properly compare arrays, so we pass a string representation to compare
+  }, [steamIdHashForComparison]);
 
   return (
     <div className="app">
-      <header className="header"></header>
-      <section className="content">
-        <div className="fullheight">
-          <div className="spinner">
-            {fireEmoji()} STEAM VENN DIAGRAM {fireEmoji()}
-          </div>
-          <p className="after-spinner">yes hello welcome</p>
-          <p>enter steam IDs below</p>
-          <div className="steamid-container">
-            {steamIds.map(steamId => (
-              <div className="steamid">
-                <input className="steamid-input" type="text" value={steamId} />
-                <button className="delete-button">
-                  <img
-                    src={deleteIcon}
-                    className="delete-button-icon"
-                    alt="delete"
-                  />
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-        {steamIds && steamIds.map(entry => <div>SteamID: {entry}</div>)}
-        {apiData &&
-          apiData.map(entry => (
-            <div>
-              {entry.map(row => (
-                <div>{row.name}</div>
-              ))}
-            </div>
-          ))}
-        <div></div>
-      </section>
+      <LeftNav steamIds={steamIds} />
+      {steamIds.map(steamId => (
+        <SteamIDHeader key={steamId} steamId={steamId} />
+      ))}
+      {steamIds.map(steamId => (
+        <GamesList key={steamId} games={state.userData[steamId]} />
+      ))}
+      <div></div>
     </div>
   );
 }
+
+const GamesList = props => (
+  <div>
+    {props.games.map(game => (
+      <GameEntry key={`${game.name}-${props.key}`} game={game} />
+    ))}
+  </div>
+);
+
+const GameEntry = props => <div>{props.game.name}</div>;
+
+const SteamIDHeader = props => <div>SteamID: {props.steamId}</div>;
+
+const LeftNav = props => (
+  <header className="fullheight">
+    <div className="spinner">
+      {fireEmoji()} STEAM VENN DIAGRAM {fireEmoji()}
+    </div>
+    <p className="after-spinner">yes hello welcome</p>
+    <p>enter steam IDs below</p>
+    <div className="steamid-container">
+      {Object.keys(props.steamIds).map(key => (
+        <div className="steamid">
+          <input
+            className="steamid-input"
+            type="text"
+            value={props.steamIds[key]}
+          />
+          <button className="delete-button">
+            <img src={deleteIcon} className="delete-button-icon" alt="delete" />
+          </button>
+        </div>
+      ))}
+    </div>
+  </header>
+);
 
 const fireEmoji = () => (
   <span role="img" aria-label="fire emoji">
     🔥
   </span>
 );
+
+const queryAPIForGamesOwned = async (steamIdList, dispatch) => {
+  for (const steamId of steamIdList) {
+    const apiResponse = await service.getOwnedGames(steamId);
+    dispatch({ type: "setApiData", data: { steamId, apiResponse } });
+  }
+};
 
 export default App;
